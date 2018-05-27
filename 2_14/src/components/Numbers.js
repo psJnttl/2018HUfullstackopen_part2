5734c819c2988c78a9ada33d1b62f3a29bac509c
Pasi Junttila <pasi.junttila@live.com>
2018-05-27 18:40:21 +0300

    Tehtava 2.14 ratkaisu.

---
+++ 2_14/src/components/Numbers.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Numbers = ({numbers, constraint}) => {
+  const constrained = numbers.filter((item) => {
+    const index = item.name.toLowerCase().indexOf(constraint);
+    return index === -1 ? false : true;
+  });
+  const toBeShown = constrained.map((number) => {
+    return <tr key={number.name}><td>{number.name}</td><td> {number.number}</td></tr>}
+  );
+  return (
+    <div>
+      <table>
+        <tbody>
+          {toBeShown}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+export default Numbers;
